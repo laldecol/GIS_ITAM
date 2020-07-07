dd64b384b6942dff77cc625066c6f7c3dbfc0f78
Lorenzo Aldeco <lorenzo_aldeco_leo@brown.edu>
2020-07-06 21:10:09 -0400

    Progress cleaning input dbf

--- Codigo/EOD/prep_eod.py
+++ Codigo/EOD/prep_eod.py
@@ -16,6 +16,16 @@ def dbf_to_df(path):
     frame = DataFrame(iter(dbf))
     return frame
     
+def clean_viaje_df(df):
+    var_dict={'P5_6':'ori_lug','P5_7_6':'ori_del','P5_7_7':'ori_ent', 'DTO_ORIGEN':'ori_dto',
+              'P5_11A':'des_lug','P5_12_6':'des_del','P5_12_7':'des_ent', 'DTO_DEST':'des_dto',
+              'P5_9_1':'ini_hor','P5_9_2':'ini_min','P5_10_1':'fin_hor','P5_10_2':'fin_min'}
+              #'P5_14_01':'dummy_auto','P5_15_01':'num_auto',
+              #'P5_14_01':'dummy_auto','P5_15_01':'num_auto',
+              #'P5_14_01':'dummy_auto','P5_15_01':'num_auto',}
+    df=df.rename(columns=var_dict, inplace=True)
+    
+    
 if __name__ == "__main__":
     
     runfile=pathlib.Path(__file__).absolute()
